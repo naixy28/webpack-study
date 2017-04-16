956fc2d0188d4939659d8aca54bf7aa44383d650
naixy28 <naixy28@gmail.com>
2017-04-16 17:50:36 +0800

    8

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+var CommonChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 
 module.exports = {
-    // entry: './src/main.js',
-    entry: [
-        'babel-polyfill',
-        './src/main.js'
-    ],
+    entry: {
+        vendor: [ 'babel-polyfill', 'lodash'],
+        main: './src/main.js'
+    },
     output: {
         path: path.resolve( __dirname, './dist'),
         filename: 'bundle.js'
@@ -36,6 +36,10 @@ module.exports = {
             mangle: {screw_ie8 : true},
             compress: { screw_ie8: true, warnings: false },
             comments: false
+        }),
+        new CommonChunkPlugin({
+            name: 'vendor',
+            filename: 'vendor.bundle.js'
         })
     ]
 };
