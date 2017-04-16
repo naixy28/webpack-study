e08377f47a2ff5a54923f316b8b96c8195c14202
naixy28 <naixy28@gmail.com>
2017-04-16 17:00:36 +0800

    init 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/main.js',
+    // entry: './src/main.js',
+    entry: [
+        'babel-polyfill',
+        './src/main.js'
+    ],
     output: {
         path: path.resolve( __dirname, './dist'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            { 
+                test: /\.jsx?$/, 
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    // plugins: ['transform-runtime'],
+                    presets: ['es2015']
+                }
+            }
+        ]
     }
 };
 
